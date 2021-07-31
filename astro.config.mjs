72cb2ed26f295dd761c6599d9b042898239e151f
Rio Purnomo <mulai.dev@gmail.com>
2021-08-01 03:16:52 +0700

    setup config

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  buildOptions: {
+    site: 'https://mulai.dev',
+    sitemap: true,
+  },
+  devOptions: {
+    tailwindConfig: './tailwind.config.js',
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
