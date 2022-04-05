95fc67515057f06745a0910b8fed6e301fa9081b
Rio Purnomo <rio.purnomo@wundermanthompson.com>
2022-04-06 00:15:17 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}'],
-  darkMode: false,
+  content: ['./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}'],
   theme: {
     fontFamily: {
       'body': '"Inter", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
