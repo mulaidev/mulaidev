0618d232e127ba56ab4509a7b82d2aeb18fb3b54
Rio Purnomo <rio.purnomo@wundermanthompson.com>
2022-04-05 23:49:42 +0700

    upgrade Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-export default {
-  buildOptions: {
-    site: 'https://mulai.dev',
-    sitemap: true,
-  },
-  devOptions: {
-    tailwindConfig: './tailwind.config.js',
-  },
-};
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  site: 'https://mulai.dev',
+  integrations: [tailwind()]
+})
